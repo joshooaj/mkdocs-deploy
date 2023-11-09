1498255765d5b119e8b73da169cb3125134d69b2
Josh Hendricks <joshooaj@gmail.com>
2023-11-09 22:32:58 +0000

    test static assigned output values

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,17 @@ export async function run(): Promise<void> {
   try {
     //let siteName = core.getInput('site_name')
     //if (siteName === '') {
-    let siteName = github.context.repo.repo
+    //let siteName = github.context.repo.repo
     //}
 
     //let siteUrl = core.getInput('site_url')
     //if (siteUrl === '') {
-    let siteUrl = `https://${github.context.repo.owner}.github.io/${github.context.repo.repo}/`
+    //let siteUrl = `https://${github.context.repo.owner}.github.io/${github.context.repo.repo}/`
     //}
 
     core.debug(`site_name: ${core.getInput('site_name')}`)
-    core.setOutput('site-name', siteName)
-    core.setOutput('site-url', siteUrl)
+    core.setOutput('site-name', github.context.repo.repo)
+    core.setOutput('site-url', 'https://joshooaj.github.io/mkdocs-deploy/')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
